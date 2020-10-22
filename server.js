6d318d9cd162132b23e11367e3d6b7c8d8aea8ae
Alkawe <alixkwebb@gmail.com>
2020-10-21 23:53:13 -0400

    added config parameters

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
-//const db = require("./models");
+
 const app = express();
 
 app.use(logger("dev"));
@@ -12,7 +12,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }    
+);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
